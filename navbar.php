122c4d30f3dfe9c0194f4eca608ef3a69a5530c2
Neha <nehadumane312@gmail.com>
2021-11-17 16:33:46 +0530

    Initial commit

---
+++ navbar.php
@@ -0,0 +1,29 @@
+<!-- navbar --> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.html"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="index.html">HOME</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="transactionhistory.php">HISTORY</a>
+        </li>
+      </div>
+    </nav>
+  </body>
+  </html>
